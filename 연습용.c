8eacfc99c75366d41f1b45fb57f5e35af4c8cee0
OWSLA-park <minkipark96@gmail.com>
2019-10-22 23:28:58 +0900

    practice 3

--- 연습용.c
+++ 연습용.c
@@ -1,25 +1,42 @@
-﻿#include <stdio.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
 
-unsigned char GetBit(unsigned char dest_data, unsigned char bit_num)
+int main()
 {
-	unsigned char bit_state = 0;/*한글테스트*/
-	if (bit_num < 8) {
-		bit_state = dest_data & (0x01 << bit_num);
-		bit_state = bit_state >> bit_num;
-	}
-	return bit_state;
-}
+	int decimal = 300;                // 10진수 300
+	char hexadecimal[20] = { 0, };    // 16진수로 된 문자열을 저장할 배열
 
-void main()
-{
-	unsigned char lamp_state = 0x75;
-	unsigned char bit_state;
-	int i;
+	int position = 0;
+	while (1)
+	{
+		int mod = decimal % 16;    // 16으로 나누었을 때 나머지를 구함
+		if (mod < 10) // 나머지가 10보다 작으면
+		{
+			// 숫자 0의 ASCII 코드 값 48 + 나머지
+			hexadecimal[position] = 48 + mod;
+		}
+		else    // 나머지가 10보다 크거나 같으면
+		{
+			// 나머지에서 10을 뺀 값과 영문 대문자 A의 ASCII 코드 값 65를 더함
+			hexadecimal[position] = 65 + (mod - 10);
+		}
 
-	printf("%X -> ", lamp_state);
-	for (i = 0; i < 8; i++) {
-		bit_state = GetBit(lamp_state, 7 - i);
-		printf("%d", bit_state);
+		decimal = decimal / 16;    // 16으로 나눈 몫을 저장
+
+		position++;    // 자릿수 변경
+
+		if (decimal == 0)    // 몫이 0이되면 반복을 끝냄
+			break;
 	}
+
+	// 
+	for (int i = position - 1; i >= 0; i--)
+	{
+		printf("%c", hexadecimal[i]);
+	}
+
 	printf("\n");
+
+	return 0;
 }
