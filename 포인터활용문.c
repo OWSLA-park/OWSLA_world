1428b04adb62ec25fd6bdb59fa973096691e9821
OWSLA-park <minkipark96@gmail.com>
2019-11-13 19:03:01 +0900

    new_study

---
+++ 포인터활용문.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+void main()
+{
+	int t = 0;
+	char a = 0x12, b = 0x34;
+	short c = 0x5678;
+	short d = 0;
+	char* add = (char*)& d;
+	*add = b;
+	*(add + 1) = a;
+	short *temp = (short*)& t;
+	*temp = c;
+	*(temp + 1) = (short*)d;
+	printf("%X", t);
+}
